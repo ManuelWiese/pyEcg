aa64b593ee04bf02c5028d64a77b479cb4455bd8
Manuel Wiese <guitarfreak@gmx.net>
2018-05-13 00:03:17 +0200

    Added arduino code to project.

---
+++ arduino/ecg_5ms.ino
@@ -0,0 +1,17 @@
+void setup() {
+  pinMode(10,INPUT);
+  pinMode(11,INPUT);
+
+  Serial.begin(38400);
+}
+
+void loop() {
+  if((digitalRead(10)==1)||(digitalRead(11)==1)){
+      Serial.println("Gagal");
+  }
+  else{
+      Serial.println(analogRead(A1));
+  }
+  delay(5);
+}
+
